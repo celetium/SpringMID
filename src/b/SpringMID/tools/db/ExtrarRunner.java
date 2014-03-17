b94d0d4e1c1d557657de9baddeb9eca04713b916
celetium <celetium@gmail.com>
2014-03-17 22:44:44 +0800

    略改

--- src/b/SpringMID/tools/db/ExtrarRunner.java
+++ src/b/SpringMID/tools/db/ExtrarRunner.java
@@ -30,7 +30,7 @@ public class ExtrarRunner {
 	}
 	public static void main(String[] args) {
 		if (args.length < 1)
-			throw new RuntimeException("用法：Runner <服务器标识> [<配置文件>]");
+			throw new RuntimeException("用法：ExtrarRunner <服务器标识> [<配置文件>]");
 		String conf = (args.length > 1) ? args[1] : null;
 		_instance.run(conf, args[0]);
 	}
